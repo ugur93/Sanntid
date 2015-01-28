ddd5ac42b75c08e1b6dc270af4f251829290207a
ugur93 <cenar93@gmail.com>
2015-01-28 08:40:25 +0100

    First file

---
+++ TCP.go
@@ -0,0 +1,34 @@
+package main
+
+import(
+	"fmt"
+	"net"
+	"bufio"
+
+)
+
+
+func main(){
+	fmt.Println("hello");
+	conn, err := net.Dial("tcp", ":33546")
+	if err != nil {
+        	//fmt.Println(err)
+
+	}
+	connbuf := bufio.NewReader(conn)
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	for{
+	    str, err := connbuf.ReadString('\n')
+	    if len(str)>0 {
+		fmt.Println(str)
+	    }
+	    if err!= nil {
+		break
+	    }
+	}
+    
+
+
+
+
+}
