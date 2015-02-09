68b6f6239d52b731ea2811e80201f954f9befaab
ugur93 <cenar93@gmail.com>
2015-02-09 18:22:10 +0100

    Små endringer i IO.go

--- driver/io.go
+++ driver/io.go
@@ -1,19 +1,31 @@
 package driver
-
 /*
 #cgo LDFLAGS: -lcomedi -lm
-#include "io.h
+#include "io.h"
 */
 import "C"
 
 func IO_init() bool {
-
-	return bool(int(C.io_init())!= 1)
-
+	return bool(int(C.io_init())== 1)
 }
 
 func IO_set_bit(channel int){
 	C.io_set_bit(C.int(channel))
+}
 
+func IO_clear_bit(channel int){
+	C.io_clear_bit(C.int(channel))
+}
 
+func IO_write_analog(channel int,value int){
+	C.io_write_analog(C.int(channel),C.int(value))
+}
+
+func IO_read_bit(channel int) int{
+
+	return int(C.io_read_bit(C.int(channel)))	
+}
+
+func IO_read_analog(channel int) int{
+	return int(C.io_read_analog(C.int(channel)))
 }
