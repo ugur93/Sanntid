5c8ac3855d7636a2fdcb0f54b5af108da7ee62a8
ugur93 <cenar93@gmail.com>
2015-02-10 21:20:38 +0100

    Endring på driveren med store bokstaver

--- driver/driver.go
+++ driver/driver.go
@@ -14,7 +14,7 @@ const (
 
 )
 
-func set_motor_direction(DIRN int){
+func Set_motor_direction(DIRN int){
 	if DIRN == 0 {
 		IO_write_analog(MOTOR,0)
 	} else if DIRN>0 {
@@ -28,21 +28,22 @@ func set_motor_direction(DIRN int){
 
 }
 
-func get_obstruction_signal() (int) {
+func Get_obstruction_signal() (int) {
 	return IO_read_bit(OBSTRUCTION)
 
 }
-func get_stop_signal() (int) {
+func Get_stop_signal() (int) {
 	return IO_read_bit(STOP)
 }
-func set_stop_lamp(value int) {
-	if value {
+func Set_stop_lamp(value int) {
+	if value==1{
 		IO_set_bit(LIGHT_STOP)
 	}else{
 		IO_clear_bit(LIGHT_STOP)
 	}
 }
-func get_floor_sensor_signal() int {
+/*
+func Get_floor_sensor_signal() int {
 	if IO_read_bit(SENSOR_FLOOR1) {
 		return 0
 	}else if IO_read_bit(SENSOR_FLOOR2) {
@@ -56,7 +57,7 @@ func get_floor_sensor_signal() int {
 	}
 }
 
-func set_floor_indicator(floor int) {
+func Set_floor_indicator(floor int) {
 
 	if floor & 0x02 {
 		IO_set_bit(LIGHT_FLOOR_IND1)
@@ -64,3 +65,4 @@ func set_floor_indicator(floor int) {
 		IO_clear_bit(LIGHT_FLOOR_IND1)
 	}
 }
+*/
--- maintest.go
+++ maintest.go
@@ -7,4 +7,6 @@ import "./driver"
 
 func main() {
 	fmt.Println("test")
+	driver.Set_stop_lamp(1);
+	driver.Set_motor_direction(1);
 }
